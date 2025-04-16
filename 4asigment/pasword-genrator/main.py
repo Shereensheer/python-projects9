df44078cb7ad20d087c4ab86280fceead6efde08
Shereensheer <165431743+Shereensheer@users.noreply.github.com>
2025-04-16 21:28:44 +0500

    Add files via upload

---
+++ 4asigment/pasword-genrator/main.py
@@ -0,0 +1,20 @@
+
+import random
+
+print('welcome To your password Generator')
+
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@E$%^&*().,?0123456789'
+
+number = input('Amount of passwords to generate: ')
+number = int(number)
+
+lenght = input('Your password lenght: ')
+lenght = int(lenght)
+
+print('\nhere are your paswords:')
+
+for pwd in range(number):
+  passwords = ''
+  for c in range(lenght):
+    passwords += random.choice(chars)
+  print(passwords)
